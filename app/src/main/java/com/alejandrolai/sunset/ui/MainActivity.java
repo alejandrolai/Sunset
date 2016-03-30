b6cd99d195c2d3cee81f1b5826ab044d6b4b17b6
Alejandro <lai91992@mail.sfsu.edu>
2016-03-30 00:19:49 -0700

    Add permission check

--- app/src/main/java/com/alejandrolai/sunset/ui/MainActivity.java
+++ app/src/main/java/com/alejandrolai/sunset/ui/MainActivity.java
@@ -1,16 +1,25 @@
 package com.alejandrolai.sunset.ui;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -22,6 +31,11 @@ import com.alejandrolai.sunset.weather.Current;
 import com.alejandrolai.sunset.weather.Day;
 import com.alejandrolai.sunset.weather.Forecast;
 import com.alejandrolai.sunset.weather.Hour;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -38,7 +52,8 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     public static final String FORECASTIO_API_KEY = BuildConfig.FORECASTIO_API_KEY;
     public static final String GOOGLE_MAPS_API_KEY = BuildConfig.GOOGLE_MAPS_API_KEY;
@@ -48,14 +63,30 @@ public class MainActivity extends AppCompatActivity {
     public static final String HOURLY_FORECAST = "HOURLY_FORECAST";
     private Forecast mForecast;
 
-    @Bind(R.id.timeLabel) TextView mTimeLabel;
-    @Bind(R.id.temperatureLabel) TextView mTemperatureLabel;
-    @Bind(R.id.humidityValue) TextView mHumidityValue;
-    @Bind(R.id.precipValue) TextView mPrecipValue;
-    @Bind(R.id.summaryLabel) TextView mSummaryLabel;
-    @Bind(R.id.iconImageView) ImageView mIconImageView;
-    @Bind(R.id.refreshImageView) ImageView mRefreshImageView;
-    @Bind(R.id.progressBar) ProgressBar mProgressBar;
+    @Bind(R.id.timeLabel)
+    TextView mTimeLabel;
+    @Bind(R.id.temperatureLabel)
+    TextView mTemperatureLabel;
+    @Bind(R.id.humidityValue)
+    TextView mHumidityValue;
+    @Bind(R.id.precipValue)
+    TextView mPrecipValue;
+    @Bind(R.id.summaryLabel)
+    TextView mSummaryLabel;
+    @Bind(R.id.iconImageView)
+    ImageView mIconImageView;
+    @Bind(R.id.refreshImageView)
+    ImageView mRefreshImageView;
+    @Bind(R.id.progressBar)
+    ProgressBar mProgressBar;
+    @Bind(R.id.locationLabel)
+    EditText mLocationLabel;
+
+    private double latitude;
+    private double longitude;
+
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,23 +96,99 @@ public class MainActivity extends AppCompatActivity {
 
         mProgressBar.setVisibility(View.INVISIBLE);
 
-        final double latitude = 37.8267;
-        final double longitude = -122.423;
-
+        buildGoogleApiClient();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+            checkPermissions();
+        }
         mRefreshImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getForecast(latitude,longitude);
+                checkPermissions();
             }
         });
-        getForecast(latitude, longitude);
+        mLocationLabel.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    Toast.makeText(getApplicationContext(),mLocationLabel.getText().toString(),Toast.LENGTH_SHORT).show();
+                    getLocationFromUser(mLocationLabel.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
 
-    private void getForecast(double latitude, double longitude){
+    public void getLocationFromUser(String location) {
+        // TODO Parse string location to latitude and longitude using geocoding api
 
-        String url = "https://api.forecast.io/forecast/" + FORECASTIO_API_KEY + "/" + latitude + "," + longitude;
+    }
+
+    private void checkPermissions() {
+        int permission;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            permission = checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                    showMessage("Sunset needs to access your location",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    switch (which) {
+                                        case -1:
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                                        1);
+                                            }
+                                            break;
+                                        case -2:
+                                            showMessage("Sunset won't be able to access your location's weather", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+
+                                                }
+                                            });
+                                            break;
+                                    }
+                                }
+                            });
+                }
+            } else {
+                mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                if (mLastLocation != null) {
+                    latitude = mLastLocation.getLatitude();
+                    longitude = mLastLocation.getLongitude();
+                } else {
+                    LocationRequest mRequest = new LocationRequest();
+                    mRequest.setInterval(500);
+                    mRequest.setExpirationDuration(10000);
+                    mRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                }
+
+            }
+        } else {
+            // API is < 23 so permissions were granted at installation
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+        }
+        if (latitude != 0 && longitude != 0) {
+            getForecast(latitude,longitude);
+        }
+    }
+
+    private void getForecast(double newLatitude, double newLongitude) {
+
+        Log.d("MainActivity.getForecas","Latitude:" + newLatitude + ", longitude: " + newLongitude);
 
         if (isNetworkAvailable()) {
+
+            String url = "https://api.forecast.io/forecast/" + FORECASTIO_API_KEY + "/" + newLatitude + "," + newLongitude;
+
             toggleRefresh();
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
@@ -122,16 +229,14 @@ public class MainActivity extends AppCompatActivity {
                         } else {
                             alertUserAboutError();
                         }
-                    } catch (IOException e) {
-
-                    } catch (JSONException e) {
+                    } catch (IOException | JSONException e) {
                         e.printStackTrace();
                     }
 
                 }
             });
         } else {
-            Toast.makeText(this, R.string.network_unavailable_message,Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
         }
     }
 
@@ -149,9 +254,9 @@ public class MainActivity extends AppCompatActivity {
     private void updateDisplay() {
         Current current = mForecast.getCurrent();
 
-        mTemperatureLabel.setText(current.getTemperature() + "");
+        mTemperatureLabel.setText(Integer.toString(current.getTemperature()));
         mTimeLabel.setText("At " + current.getFormattedTime() + " it will be");
-        mHumidityValue.setText(current.getHumidity() + "");
+        mHumidityValue.setText(Double.toString(current.getHumidity()));
         mPrecipValue.setText(current.getPrecipChance() + "%");
         mSummaryLabel.setText(current.getSummary());
 
@@ -177,7 +282,7 @@ public class MainActivity extends AppCompatActivity {
 
         Hour[] hours = new Hour[data.length()];
 
-        for (int i=0;i<data.length();i++) {
+        for (int i = 0; i < data.length(); i++) {
             JSONObject jsonHour = data.getJSONObject(i);
             Hour hour = new Hour();
 
@@ -199,7 +304,7 @@ public class MainActivity extends AppCompatActivity {
 
         Day[] days = new Day[data.length()];
 
-        for (int i=0;i<data.length();i++) {
+        for (int i = 0; i < data.length(); i++) {
             JSONObject jsonDay = data.getJSONObject(i);
             Day day = new Day();
 
@@ -254,7 +359,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void alertUserAboutError() {
         AlertDialogFragment dialog = new AlertDialogFragment();
-        dialog.show(getFragmentManager(),"error_dialog");
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 
     public boolean isNetworkAvailable() {
@@ -267,14 +372,14 @@ public class MainActivity extends AppCompatActivity {
         return isAvailable;
     }
 
-    @OnClick (R.id.dailyButton)
+    @OnClick(R.id.dailyButton)
     public void startDailyActivity(View view) {
         Intent intent = new Intent(this, DailyForecastActivity.class);
         intent.putExtra(DAILY_FORECAST, mForecast.getDailyForecast());
         startActivity(intent);
     }
 
-    @OnClick (R.id.hourlyButton)
+    @OnClick(R.id.hourlyButton)
     public void startHourlyActivity(View view) {
         Intent intent = new Intent(this, HourlyForecastActivity.class);
         intent.putExtra(HOURLY_FORECAST, mForecast.getHourlyForecast());
@@ -282,4 +387,73 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+            getForecast(latitude,longitude);
+            LocationServices.FusedLocationApi.removeLocationUpdates(
+                    mGoogleApiClient, this);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    private void showMessage(String message, DialogInterface.OnClickListener listener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", listener)
+                .setNegativeButton("Cancel", listener)
+                .create()
+                .show();
+    }
+
+    /**
+     * Builds a GoogleApiClient. Uses {@code #addApi} to request the LocationServices API.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        checkPermissions();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                checkPermissions();
+                break;
+        }
+    }
 }
